dd49c2b55b0ea689215f1819967ab5517e556a05
ninatom <ntomasev@usc.edu>
2023-05-09 05:10:21 -0700

    fgh

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-const eyeball = document.querySelector('.eyeball');
-const pupil = document.createElement('div');
-pupil.classList.add('pupil');
-eyeball.appendChild(pupil);
+const eye = document.getElementById('eye');
+const eyeball = document.getElementById('eyeball');
 
-document.addEventListener('mousemove', function (event) {
+eye.addEventListener('mousemove', function(event) {
   const x = event.clientX;
   const y = event.clientY;
+  const eyeBox = eye.getBoundingClientRect();
+  const eyeCenterX = eyeBox.left + eyeBox.width / 2;
+  const eyeCenterY = eyeBox.top + eyeBox.height / 2;
+  const angle = Math.atan2(y - eyeCenterY, x - eyeCenterX);
+  const radius = eyeBox.width / 2 - eyeball.clientWidth / 2;
+  const newX = radius * Math.cos(angle);
+  const newY = radius * Math.sin(angle);
 
-  const eyeballRect = eyeball.getBoundingClientRect();
-  const eyeballCenterX = eyeballRect.left + eyeballRect.width / 2;
-  const eyeballCenterY = eyeballRect.top + eyeballRect.height / 2;
-
-  const deltaX = x - eyeballCenterX;
-  const deltaY = y - eyeballCenterY;
-
-  const angle = Math.atan2(deltaY, deltaX);
-  const radius = eyeballRect.width / 2 - pupil.getBoundingClientRect().width / 2;
-
-  const pupilX = eyeballCenterX + Math.cos(angle) * radius;
-  const pupilY = eyeballCenterY + Math.sin(angle) * radius;
-
-  pupil.style.transform = `translate(${pupilX}px, ${pupilY}px)`;
+  eyeball.style.transform = `translate(${newX}px, ${newY}px)`;
 });
